f9befa495c27a9524f873065da7faeadabd3047b
Shell1010 <amin.dev03@gmail.com>
2024-10-24 23:44:22 +0000

    Forgot to add token auth :dem:

--- src/client.rs
+++ src/client.rs
@@ -9,18 +9,22 @@ pub type Client = DiscordClient;
 pub struct DiscordClient {
     pub client: reqwest::Client,
     pub proxy_info: Option<url::Url>,
+    pub token: String,
 }
 
 impl DiscordClient {
-    pub async fn new() -> DiscordClient {
+    pub async fn new(token: &str) -> DiscordClient {
         // I have no idea atp, pls fix
         let mut real_headers = default_headers(None);
         let other_headers = experiment_headers(reqwest::Client::new()).await;
         merge_headermaps(&mut real_headers, other_headers);
+
         let client = reqwest::Client::builder()
+            .default_headers(real_headers)
             // You originally used the default implementation for rewqest::cookie::Jar
             // Pretty much does the same thing
-            .default_headers(real_headers)
+            // From what I read in docs you basically don't have to pass cookie argument yourself
+            // Though ig might as well do it initially though
             .cookie_store(true)
             .use_rustls_tls()
             .build()
@@ -28,6 +32,7 @@ impl DiscordClient {
 
         Self {
             client,
+            token: token.to_string(),
             proxy_info: None,
         }
     }
@@ -36,14 +41,14 @@ impl DiscordClient {
 impl DiscordClient {
     pub async fn send_request(&self, request: HttpRequest) -> Result<Response, Box<dyn Error>> {
         let builder = request.to_request_builder(&self.client);
-        let response = builder.send().await?;
+        let response = builder.header("authorization", &self.token).send().await?;
         Ok(response)
     }
 }
 
 #[tokio::test]
 async fn request_builder_test() {
-    let client = DiscordClient::new().await;
+    let client = DiscordClient::new("abcd1234").await;
     client
         .send_request(HttpRequest::Get {
             endpoint: "/users/@me".to_string(),
