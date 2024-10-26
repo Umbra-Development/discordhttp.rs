f656b7b21e3bf8b11a9a9c7276a844e1c7c2489a
Shell1010 <amin.dev03@gmail.com>
2024-10-26 21:29:35 +0000

    testijoaiwjdoijaowdiaocnai

--- src/client.rs
+++ src/client.rs
@@ -9,6 +9,8 @@ use reqwest::{
     header::{HeaderMap, HeaderName, HeaderValue},
     Response,
 };
+use rustls::ClientConfig;
+use std::any::Any;
 use std::error::Error;
 
 pub type Client = DiscordClient;
@@ -25,7 +27,8 @@ impl DiscordClient {
         let mut real_headers = default_headers(None);
         let other_headers = experiment_headers(reqwest::Client::new()).await;
         merge_headermaps(&mut real_headers, other_headers);
-
+        // let val = (&mut tls as &mut dyn Any).downcast_mut::<Option<ClientConfig>>();
+        // println!("{val:#?} OUTSIDE THE LIB");
         let client = reqwest::Client::builder()
             .default_headers(real_headers)
             // You originally used the default implementation for rewqest::cookie::Jar
@@ -51,6 +54,28 @@ impl DiscordClient {
         let response = builder.send().await?;
         Ok(response)
     }
+}
+
+#[tokio::test]
+async fn rustls_sucks() {
+    let tls = create_custom_tls_config();
+
+    fn func(tls: impl Any + std::fmt::Debug) {
+        let mut tls = Some(tls);
+        println!(
+            "{:?}",
+            (&mut tls as &mut dyn Any).is::<Option<ClientConfig>>()
+        );
+        println!("{}", std::any::type_name::<ClientConfig>());
+        if let Some(tls) = (&mut tls as &mut dyn Any).downcast_mut::<Option<rustls::ClientConfig>>()
+        {
+            println!("GOT SOME",);
+        } else {
+            println!("GOT NONE",);
+        }
+    }
+
+    func(tls);
 }
 
 #[tokio::test]
