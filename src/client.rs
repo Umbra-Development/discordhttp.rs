380e722857e43422cf9e0e2d03395b415ccff060
Shell1010 <amin.dev03@gmail.com>
2024-10-30 15:45:54 +0000

    Custom Pre-config, working on it

--- src/client.rs
+++ src/client.rs
@@ -2,10 +2,15 @@
 use crate::easy_headers;
 use crate::types::HttpRequest;
 use crate::utils::{default_headers, experiment_headers, merge_headermaps};
+use rquest::boring::ssl::{SslConnector, SslVersion, SslCurve, SslMethod, SslOptions};
 // Required for macro idfk why
+use rquest::{
+    header,
+    HttpVersionPref,
+};
+use rquest::{PseudoOrder::*, SettingsOrder::*};
 
-use rquest::tls::Impersonate;
-use rquest::tls::Version;
+use rquest::tls::{chrome, Impersonate, ImpersonateSettings, TlsSettings, Http2Settings, Version};
 use rquest::{
     header::{HeaderMap, HeaderName, HeaderValue},
     Response,
@@ -33,11 +38,15 @@ impl DiscordClient {
                 .unwrap(),
         )
         .await;
+
+       
+
         merge_headermaps(&mut real_headers, other_headers);
         let client = rquest::Client::builder()
-            .impersonate(Impersonate::Chrome128)
+            .impersonate_without_headers(Impersonate::Chrome128)
             .default_headers(real_headers)
             .cookie_store(true)
+            .use_preconfigured_tls(tls_preconfig())
             .max_tls_version(Version::TLS_1_2)
             .build()
             .unwrap();
@@ -48,6 +57,56 @@ impl DiscordClient {
         }
     }
 }
+fn tls_preconfig() -> ImpersonateSettings {
+    ImpersonateSettings::builder()
+
+    .tls(
+        TlsSettings::builder()
+            .connector(Box::new(|| {
+                let mut builder = SslConnector::builder(SslMethod::tls_client())?;
+                builder.cert_store();
+                builder.set_curves(&[SslCurve::X25519, SslCurve::SECP256R1, SslCurve::SECP384R1])?;
+                
+                Ok(builder)
+            }))
+            .tls_sni(true)
+            .http_version_pref(HttpVersionPref::All)
+            .application_settings(true)
+            .pre_shared_key(true)
+            .enable_ech_grease(true)
+            .permute_extensions(true)
+            .min_tls_version(Version::TLS_1_2)
+            .max_tls_version(Version::TLS_1_2)
+            .build(),
+    )
+    .http2(
+        Http2Settings::builder()
+            .initial_stream_window_size(6291456)
+            .initial_connection_window_size(15728640)
+            .max_concurrent_streams(1000)
+            .max_header_list_size(262144)
+            .header_table_size(65536)
+            .enable_push(false)
+            .headers_priority((0, 255, true))
+            .headers_pseudo_order([Method, Scheme, Authority, Path])
+            .settings_order(&[
+                HeaderTableSize,
+                EnablePush,
+                MaxConcurrentStreams,
+                InitialWindowSize,
+                MaxFrameSize,
+                MaxHeaderListSize,
+                EnableConnectProtocol,
+            ])
+            .build(),
+    )
+    .headers(Box::new(|headers| {
+        headers.insert(header::USER_AGENT, HeaderValue::from_static("rquest"));
+    }))
+    .build()
+
+}
+
 
 impl DiscordClient {
     pub async fn send_request(&self, request: HttpRequest) -> Result<Response, Box<dyn Error>> {
@@ -61,7 +120,6 @@ impl DiscordClient {
 #[tokio::test]
 async fn request_builder_test() {
     let client = DiscordClient::new().await;
-    for i in 0..20 {
         let response = client
             .send_request(HttpRequest::Get {
                 endpoint: "/experiments".to_string(),
@@ -71,7 +129,6 @@ async fn request_builder_test() {
             .await
             .unwrap();
         println!("{}", response.text().await.unwrap());
-    }
 }
 
 
