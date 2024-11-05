f44725f1e474b795e1c02bb5f66217de458bfb08
Shell1010 <amin.dev03@gmail.com>
2024-11-05 23:08:08 +0000

    brkoen

--- src/client.rs
+++ src/client.rs
@@ -43,12 +43,11 @@ impl DiscordClient {
         merge_headermaps(&mut real_headers, other_headers);
 
         let builder = rquest::Client::builder()
-            .impersonate_without_headers(Impersonate::Chrome128)
             .default_headers(real_headers)
             .cookie_store(true)
             .use_preconfigured_tls(tls_preconfig())
-            .pre_shared_key()
             .max_tls_version(Version::TLS_1_2);
+
 
         let client = configure_proxy(builder, proxy)
             .build()
@@ -79,6 +78,17 @@ fn tls_preconfig() -> ImpersonateSettings {
                 let mut builder = SslConnector::builder(SslMethod::tls_client())?;
                 builder.cert_store();
                 builder.set_curves(&[SslCurve::X25519, SslCurve::SECP256R1, SslCurve::SECP384R1])?;
+                builder.set_alpn_protos(b"\x08http/1.1\x02h2\x03h3")?;
+                builder.set_sigalgs_list(&[ 
+                    "ecdsa_secp256r1_sha256",
+                    "rsa_pss_rsae_sha256",
+                    "rsa_pkcs1_sha256",
+                    "ecdsa_secp384r1_sha384",
+                    "rsa_pss_rsae_sha384",
+                    "rsa_pkcs1_sha384",
+                    "rsa_pss_rsae_sha512",
+                    "rsa_pkcs1_sha512",
+                ].join(":"))?;
                 builder.set_cipher_list(&[
                     "TLS_AES_128_GCM_SHA256",
                     "TLS_AES_256_GCM_SHA384",
@@ -96,38 +106,43 @@ fn tls_preconfig() -> ImpersonateSettings {
                     "TLS_RSA_WITH_AES_128_CBC_SHA",
                     "TLS_RSA_WITH_AES_128_CBC_SHA"
                 ].join(":"))?;
+                builder.set_grease_enabled(true);
+                builder.enable_ocsp_stapling();
+                builder.enable_signed_cert_timestamps();
+                builder.set_permute_extensions(true);
+                 
                 
                 Ok(builder)
             }))
-            .tls_sni(true)
-            .http_version_pref(HttpVersionPref::All)
-            .application_settings(true)
+            .http_version_pref(HttpVersionPref::Http2)
+            // .application_settings(true) // This gives me 17513 apparently
             .pre_shared_key(true)
             .enable_ech_grease(true)
             .permute_extensions(true)
-            .min_tls_version(Version::TLS_1_2)
-            .max_tls_version(Version::TLS_1_2)
+            .application_settings(true)
+            .min_tls_version(Version::TLS_1_1)
+            .max_tls_version(Version::TLS_1_3)
             .build(),
     )
     .http2(
         Http2Settings::builder()
-            .initial_stream_window_size(6291456)
-            .initial_connection_window_size(15728640)
-            .max_concurrent_streams(1000)
-            .max_header_list_size(262144)
-            .header_table_size(65536)
-            .enable_push(false)
-            .headers_priority((0, 255, true))
-            .headers_pseudo_order([Method, Scheme, Authority, Path])
-            .settings_order(&[
-                HeaderTableSize,
-                EnablePush,
-                MaxConcurrentStreams,
-                InitialWindowSize,
-                MaxFrameSize,
-                MaxHeaderListSize,
-                EnableConnectProtocol,
-            ])
+        .initial_stream_window_size(6291456)
+        .initial_connection_window_size(15728640)
+        .max_header_list_size(262144)
+        .header_table_size(65536)
+        .enable_push(false)
+        .headers_priority((0, 255, true))
+        .headers_pseudo_order([Method, Authority, Scheme, Path])
+        .settings_order(&[
+            HeaderTableSize,
+            EnablePush,
+            MaxConcurrentStreams,
+            InitialWindowSize,
+            MaxFrameSize,
+            MaxHeaderListSize,
+            UnknownSetting8,
+            UnknownSetting9,
+        ])
             .build(),
     )
     .build()
@@ -141,6 +156,11 @@ impl DiscordClient {
         let response = builder.send().await?;
         Ok(response)
     }
+
+    pub async fn send_base_request(&self, url: &str, additional_headers: HeaderMap<HeaderValue>) {
+        let client = self.client.get(url).headers(additional_headers).send().await.unwrap();
+        println!("{}", client.text().await.unwrap());
+    } 
 }
 
 // #[tokio::test]
@@ -158,48 +178,55 @@ impl DiscordClient {
 // }
 //
 #[tokio::test]
-async fn token_join_leave() {
-    let mut tokens = String::new();
-    let mut file = std::fs::File::open("./tokens.txt").unwrap();
-    file.read_to_string(&mut tokens).unwrap();
-    let vals= tokens.split('\n').into_iter();
-    
+async fn get_headers() {
     let client = DiscordClient::new(None).await;
-    for val in vals {
-        // Joining a server
-        let resp = client.send_request(
-            HttpRequest::Post {
-                endpoint: "/invites/PuJMXFPm".to_string(),
-                body: Some(json!({"session_id": Value::Null})),
-                additional_headers: Some(easy_headers!({"authorization": val}))
-            }
-        ).await
-        .unwrap();
-        println!("{}", resp.status().as_str());
-        if resp.status().is_success() {
-            println!("Token has joined the server: {}", val)
-        } else {
-            println!("Token could not join the server: {}\n{}\n{}", val, resp.status().as_str(), resp.text().await.unwrap())
-        }
-
-        // // Leaving a server
-        // let resp = client.send_request(
-        //     HttpRequest::Delete {
-        //         endpoint: "/users/@me/guilds/1299815021794164838".to_string(),
-        //         additional_headers: Some(easy_headers!({"authorization": val})) 
-        //     }
-        // )
-        // .await
-        // .unwrap();
-        // println!("{}", resp.status().as_str());
-        // if resp.status().is_success() {
-        //     println!("Token has left the server: {}", val)
-        // } else {
-        //     println!("Token could not leave the server: {}\n{}\n{}", val, resp.status().as_str(), resp.text().await.unwrap())
-        // }
-
+    for _i in 0..10 {
+        client.send_base_request("https://httpbin.org/headers", easy_headers!({"fake": "tA"})).await;
     }
 }
+// #[tokio::test]
+// async fn token_join_leave() {
+//     let mut tokens = String::new();
+//     let mut file = std::fs::File::open("./tokens.txt").unwrap();
+//     file.read_to_string(&mut tokens).unwrap();
+//     let vals= tokens.split('\n').into_iter();
+//     
+//     let client = DiscordClient::new(None).await;
+//     for val in vals {
+//         // Joining a server
+//         let resp = client.send_request(
+//             HttpRequest::Post {
+//                 endpoint: "/invites/PuJMXFPm".to_string(),
+//                 body: Some(json!({"session_id": Value::Null})),
+//                 additional_headers: Some(easy_headers!({"authorization": val}))
+//             }
+//         ).await
+//         .unwrap();
+//         println!("{}", resp.status().as_str());
+//         if resp.status().is_success() {
+//             println!("Token has joined the server: {}", val)
+//         } else {
+//             println!("Token could not join the server: {}\n{}\n{}", val, resp.status().as_str(), resp.text().await.unwrap())
+//         }
+//
+//         // // Leaving a server
+//         // let resp = client.send_request(
+//         //     HttpRequest::Delete {
+//         //         endpoint: "/users/@me/guilds/1299815021794164838".to_string(),
+//         //         additional_headers: Some(easy_headers!({"authorization": val})) 
+//         //     }
+//         // )
+//         // .await
+//         // .unwrap();
+//         // println!("{}", resp.status().as_str());
+//         // if resp.status().is_success() {
+//         //     println!("Token has left the server: {}", val)
+//         // } else {
+//         //     println!("Token could not leave the server: {}\n{}\n{}", val, resp.status().as_str(), resp.text().await.unwrap())
+//         // }
+//
+//     }
+// }
 
 // #[tokio::test]
 // async fn ja3_fingerprint_test() {
